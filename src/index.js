ecb4bca16b7fbe785c0ff0658263619f989c525b
Ran Yitzhaki <ranyitzhaki@gmail.com>
2017-10-03 17:11:15 +0300

    add main entry for backup 💾

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const connectMessageChannel = require('./connect/connect');
+const listenerMessageChannel = require('./listener/listener');
+
+module.exports = {
+  connectMessageChannel,
+  listenerMessageChannel,
+};
