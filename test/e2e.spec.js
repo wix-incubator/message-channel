e8cb6e899ebd224d2e06751eeb01f3bd321aaee7
Ran Yitzhaki <ranyitzhaki@gmail.com>
2017-10-15 16:38:17 +0300

    add --no-sandbox argument to the puppeteer launch function

--- test/e2e.spec.js
+++ test/e2e.spec.js
@@ -36,7 +36,7 @@ describe('e2e', () => {
   let page;
 
   before(async () => {
-    browser = await puppeteer.launch({headless: true});
+    browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
     page = await browser.newPage();
   });
 
